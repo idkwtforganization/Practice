dab273fa95dbad1fd964ade69f98515488975df4
Horryt <buzinnikita66@gmail.com>
2022-09-28 03:00:44 +0300

    Probably fixed

--- Task1/task1.c
+++ Task1/task1.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
+#include <stdlib.h>
 #define M 10
 
-char* get_s(void){
+char c;
+
+char* get_s(void)
+{
 	int n, k = 0;
 	char* s = malloc(M);
-	if (!s){
+	if (!s) 
+	{
 		free(s);
 		return NULL;
 	}
-	while (fgets(s + k, M, stdin)){
+	while (fgets(s + k, M, stdin))
+	{
 		n = strlen(s);
-		if(s[n - 1] != '\n'){
+		if (s[n - 1] != '\n')
+		{
 			k = k + M - 1;
 			s = realloc(s, k + M);
-			if (!s){
+			c = getchar();
+			if (!s) 
+			{
 				free(s);
 				return NULL;
 			}
 		}
-		else {
-			s[n - 1] = '\0';
+		else
+		{
+			s[n-1]='\0';
 			return s;
 		}
 	}
@@ -30,57 +38,64 @@ char* get_s(void){
 	return NULL;
 }
 
-char** sort(char** a, int i){
+char** sort(char** a, int i)
+{
 	short flag = 0;
 	char* c;
 	for (int j = 0; j < (i - 1); j++)
 	{
-		flag = 0;
-		for (int l = 0; l < (i - 1 - j); l++)
+		flag = 1;
+		for (int l = 0; l < (i - 1); l++)
 		{
-			if (strcmp(a[l], a[l + 1]) > 0)
+			if ((strlen(a[l]) > strlen(a[l + 1])) || ((strcmp(a[l], a[l + 1]) > 0) && (strlen(a[l]) == strlen(a[l + 1])))) 
 			{
 				c = a[l + 1];
 				a[l + 1] = a[l];
 				a[l] = c;
-				flag = 1;
+				flag = 0;
 			}
 		}
-		if (flag == 0) return a;
+		if (flag == 1) return a;
 	}
 	return a;
 }
 
-void out(char** a, int i){
-	for (int j = 0; j < i; j++){
+void out(char** a, int i)
+{
+	for (int j = 0; j < i; j++)
+	{
 		printf("%s\n", a[j]);
-  		free(a[j]);
-  	}
-    free(a);
+		free(a[j]);
+	}
+	free(a);
 }
 
-int main(){
-
-	char** mas;
+int main()
+{
 	int i = 0, k = 1;
-	mas = (char**)malloc(sizeof(char*));
-	
-  	while(1){
+	char** stringsArr;
+	stringsArr = (char**)malloc(sizeof(char*));
+	while (1)
+	{
 		char* s = get_s();
-		if (s == NULL){
+		if (s == NULL) 
+		{
 			free(s);
 			break;
 		}
-		mas[i] = s;
-		i = i + 1;
-		mas = realloc(mas, sizeof(char*) * k);	
-		k = k + 1;
+		stringsArr[i] = s;
+		k++;
+		stringsArr = realloc(stringsArr, k * sizeof(char*));
+		i++;
+		if (c == EOF){
+			free(s);
+			break;
+		}
 	}
+	stringsArr = sort(stringsArr, i);
 	
-	mas = sort(mas, i);
+	printf("\n");
 	
-  	out(mas, i);
-  	
+	out(stringsArr, i);
 	return 0;
-	
 }
